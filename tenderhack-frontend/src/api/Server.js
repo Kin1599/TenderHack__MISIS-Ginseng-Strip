c5bfd6fab331808844834a5765627d1b14de7457
Kin <dkonopliannikov05@gmail.com>
2024-04-07 08:13:59 +0300

    Fix bugs

--- tenderhack-frontend/src/api/Server.js
+++ tenderhack-frontend/src/api/Server.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export default class SendServer{
-    static async sendDataToServer(item){
-        try{
+export default class SendServer {
+    static async sendDataToServer(item) {
+        try {
             const response = await axios.post(
                 'http://127.0.0.1:8000/',
                 { nameItem: item },
@@ -12,77 +12,72 @@ export default class SendServer{
                     },
                 }
             );
-            console.log(response.data)
-            return response.data
-        } catch(error) {
-            console.error(error)
+            console.log(response.data);
+            return response.data;
+        } catch (error) {
+            console.error(error);
         }
     }
 
-    static async predictType(item){
-        try{
-            const response = await axios.get('http://localhost:8008/predict_type', 
-            {"text": item},
-            { 
-            headers: { 
-                "access-control-allow-credentials": "true", 
-                "access-control-allow-origin": "*" }}
-        );
+    static async predictType(item) {
+        try {
+            const response = await axios.get('http://localhost:8008/predict_type', {
+                data: { 'text': item },
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
             return response.data;
         } catch (error) {
             console.error('Error fetching data', error);
         }
     }
 
-    static async preprocess(item){
+    static async preprocess(item) {
         try {
-            const response = await axios.get(
-                "http://localhost:8008/preprocess",
-                { "text": item },
-                {
+            const response = await axios.get("http://localhost:8008/preprocess",
+            {
+                data: { 'text': item },
                 headers: {
                     'Content-Type': 'application/json',
                 },
-            }) 
-            console.log(response.data)
-            return response.data
-        } catch(error) {
-            console.error(error)
+            });
+            console.log(response.data);
+            return response.data;
+        } catch (error) {
+            console.error(error);
         }
     }
 
-    static async predictCategory(item){
+    static async predictCategory(item) {
         try {
-            const response = await axios.get(
-                "http://localhost:8008/predict_category", 
-                { "text": item },
-                {
+            const response = await axios.get("http://localhost:8008/predict_category",
+            {
+                data: { 'text': item },
                 headers: {
                     'Content-Type': 'application/json',
                 },
-            }) 
-            console.log(response.data)
-            return response.data
-        } catch(error) {
-            console.error(error)
+            });
+            console.log(response.data);
+            return response.data;
+        } catch (error) {
+            console.error(error);
         }
     }
 
-    static async getField(category){
+    static async getField(category) {
         try {
-            const response = await axios.get(
-                "http://localhost:8008/get_fields", 
-                { category },
-                {
+            const response = await axios.get("http://localhost:8008/get_fields",
+            {
+                data: { category },
                 headers: {
                     'Content-Type': 'application/json',
                 },
-            }) 
-            console.log(response.data)
-            return response.data
-        } catch(error) {
-            console.error(error)
+            });
+            console.log(response.data);
+            return response.data;
+        } catch (error) {
+            console.error(error);
         }
     }
 }
-
