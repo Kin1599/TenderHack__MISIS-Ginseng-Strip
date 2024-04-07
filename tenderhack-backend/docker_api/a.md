d99bd5cd376156500a30a1ade07c47c06d7b836b
rdinit <28570294+rdinit@users.noreply.github.com>
2024-04-07 10:15:50 +0300

    get -> post

---
+++ tenderhack-backend/docker_api/a.md
@@ -0,0 +1,3 @@
+ssh -N -R 8008:localhost:8008 rdinit@78.24.219.123
+
+ssh -R 8008:localhost:8008 -R -o rdinit@78.24.219.123 -N
--- tenderhack-backend/docker_api/api/app/main.py
+++ tenderhack-backend/docker_api/api/app/main.py
@@ -1,26 +1,40 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
+from time import sleep
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешаем запросы от всех источников (*), можно также указать конкретные источники ['http://localhost', 'http://localhost:3000']
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  # Разрешаем использование указанных методов
+    allow_headers=["*"],  # Разрешаем использование всех заголовков
+)
+
+def get_by_p(prompt):
+    return requests.get('http://ptp.pythonanywhere.com/get_desc').json()[prompt]
+
+def add_new(prompt):
+    data = {'prompt':prompt}
+    requests.get('http://ptp.pythonanywhere.com/generate_desc', json=data)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/preprocess")
+@app.post("/preprocess")
 def preprocess(text: dict):
     response = requests.get('http://preprocessing_model/get_pred', json=text).json()
 
     if response["text"] == 'Наименование должно состоять минимум из 3-x слов, длина наименования должна быть более 10 символов':
         response["text"] = 'Too short'
     #{"text": "Toxic text"} Too short или обработанный
-   
     return response
 
-@app.get("/predict_type")
+@app.post("/predict_type")
 def type_prediction(text: dict):
     t_type = requests.get('http://type_prediction_model/get_pred', json=text).json()['text']
     response = {'type': t_type, 'manufacturer': [], 'model': []}
@@ -34,7 +48,7 @@ def type_prediction(text: dict):
             response['manufacturer'] += parsed['manufacturer']
     return response
 
-@app.get("/predict_category")
+@app.post("/predict_category")
 def predict_category(text: dict):
     cats = requests.get('http://category_pred_model/get_pred', json=text).json()['text']
     response = []
@@ -43,7 +57,7 @@ def predict_category(text: dict):
             response.append(i)
     return response
 
-@app.get("/get_fields")
+@app.post("/get_fields")
 def get_fields(text: dict):
     cats = requests.get('http://characteristics_fields/get_pred', json=text).json()['text']
     response = []
@@ -51,3 +65,14 @@ def get_fields(text: dict):
         if i not in response:
             response.append(i)
     return cats
+
+@app.post("/generate_description")
+def generate_description(text: dict):
+    p = text['text']
+    add_new(p)
+    for i in range(20):
+        sleep(5)
+        a = get_by_p(p)
+        if a != '':
+            return {'text': a}
+    return {'text': a}
